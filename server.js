4a084cbb3134aba322d28fa413dd357e31ead3e0
Jorge Gonzalez <jorge.1014@gmail.com>
2023-09-24 20:56:23 -0700

    intalled inquirer

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3001,
+   // port: 3001,
     user: 'root',
     password: 'superman1',
     database: 'employee_tracking'
